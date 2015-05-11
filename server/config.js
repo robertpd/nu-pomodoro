df7153dff3ca8d84a14558ec1409fe00fc8ab69e
Jack Hsu <jackh@nulogy.com>
2015-05-11 16:40:34 -0400

    updated config for production

--- server/config.js
+++ server/config.js
@@ -2,5 +2,6 @@ var argv = require('yargs').default('production', false).argv;
 
 module.exports = {
   jwtSecret: 'e6f3517990a148af891379a0eea24921',
+  url: argv.production ? 'http://nu-pomodoro.herokuapp.com/' : 'http://localhost:8000',
   publicPath: argv.production ? './public/production' : './public/development'
 };
