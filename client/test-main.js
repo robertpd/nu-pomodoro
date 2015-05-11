8ad7799babc433460f789bcd0ba999e1112a4004
Jack Hsu <jackh@nulogy.com>
2015-05-10 21:46:27 -0400

    adds testing framework

---
+++ client/test-main.js
@@ -0,0 +1,21 @@
+var jsdom = require('jsdom');
+var glob = require('glob');
+
+require("babel/register")({
+  experimental: true,
+  optional: ['runtime'],
+  only: /client\/js/
+});
+
+// Setup browser environment so that we can test React components.
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.parentWindow;
+global.navigator = {
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+};
+global.console.debug = function () {}; // NodeJS does not have console.debug, but React uses it.
+
+// Load all specs.
+glob.sync(__dirname + '/**/__spec__/**/*-spec.js', {}).forEach(function (file) {
+  require(file);
+});
