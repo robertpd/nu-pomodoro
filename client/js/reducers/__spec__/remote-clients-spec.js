a5a2ebcd4675e50d8e8069ffb52e785aeed781c1
Jack Hsu <jackh@nulogy.com>
2015-08-25 09:57:47 -0400

    converts remote clients to immutable

--- client/js/reducers/__spec__/remote-clients-spec.js
+++ client/js/reducers/__spec__/remote-clients-spec.js
@@ -2,15 +2,14 @@ import * as RemoteActions from '../../actions/remote-clients';
 import remoteClients from '../remote-clients';
 import { expect } from 'chai';
 import { ActionTypes } from '../../constants';
-
-import { seq } from '../../test-utils';
+import Immutable from 'immutable';
 
 describe('Remote client reducers', () => {
   it('removes client when remoteClientRemoved is received', () => {
-    let state = [{id: 1234}];
+    let state = Immutable.fromJS([{id: 1234}]);
 
     state = remoteClients(state, { type: ActionTypes.REMOTE_CLIENT_REMOVED, id: 1234, remainingTime: 0 });
 
-    expect(state).to.be.empty;
+    expect(state.toJS()).to.be.empty;
   });
 });
