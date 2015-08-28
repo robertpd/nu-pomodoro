2527d75531823b970edd3b90a65382ad24f8b0a3
Jack Hsu <jackh@nulogy.com>
2015-08-28 10:16:15 -0400

    cleans up webpack files

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -7,34 +7,6 @@ var config = require("../make-webpack-config")({
   debug: true
 });
 
-
-//var config = {
-//  devtool: 'eval',
-//  colors: true,
-//  progress: true,
-//  entry: [
-//    'webpack-dev-server/client?http://localhost:2992',
-//    'webpack/hot/only-dev-server',
-//    './src/index'
-//  ],
-//  output: {
-//    path: path.join(__dirname, 'dist'),
-//    filename: 'bundle.js',
-//    publicPath: '/static/'
-//  },
-//  plugins: [
-//    new webpack.HotModuleReplacementPlugin(),
-//    new webpack.NoErrorsPlugin()
-//  ],
-//  module: {
-//    loaders: [{
-//      test: /\.js$/,
-//      loaders: ['react-hot', 'babel'],
-//      include: path.join(__dirname, 'src')
-//    }]
-//  }
-//};
-
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   colors: true,
--- webpack/webpack-dev-server.config.js
+++ webpack/webpack-dev-server.config.js
@@ -1,4 +1,4 @@
-module.exports = require("./make-webpack-config")({
+module.exports = require("../make-webpack-config")({
 	devServer: true,
 	devtool: "eval",
 	debug: true
