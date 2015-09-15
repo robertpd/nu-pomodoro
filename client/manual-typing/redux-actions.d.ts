45f7017295faaf8ed8e10d695f5da56f275c2bd0
Jack Hsu <jackh@nulogy.com>
2015-09-14 23:27:44 -0400

    adds type for redux

--- client/manual-typing/redux-actions.d.ts
+++ client/manual-typing/redux-actions.d.ts
@@ -1,22 +1,34 @@
-declare module 'redux-actions' {
+// Type definitions for redux-actions v0.8.0
+// Project: https://github.com/acdlite/redux-actions
+// Definitions by: Jack Hsu <https://github.com/jaysoo>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+declare module ReduxActions {
   // FSA-compliant action.
   // See: https://github.com/acdlite/flux-standard-action
-  export type Action = {
+  type Action = {
     type: string
-    payload: any
+    payload?: any
     error?: boolean
     meta?: any
   };
 
+  type PayloadCreator<T> = (...args: any[]) => T;
+  type MetaCreator = (...args: any[]) => any;
+
   type Reducer<T> = (state: T, action: Action) => T;
 
   type ReducerMap<T> = {
-    [actionType: string]: Reducer<T>
-  };
+  [actionType: string]: Reducer<T>
+};
 
-  export function createAction<T>(actionType: string, payloadCreator: Reducer<T>, metaCreator?: Reducer<T>): Reducer<T>;
+export function createAction<T>(actionType: string, payloadCreator?: PayloadCreator<T>, metaCreator?: MetaCreator): (...args: any[]) => Action;
 
-  export function handleAction<T>(actionType: string, reducer: Reducer<T>): T;
+export function handleAction<T>(actionType: string, reducer: Reducer<T> | ReducerMap<T>): Reducer<T>;
 
-  export function handleActions<T>(reducerMap: ReducerMap<T>, initialState: T): T;
+export function handleActions<T>(reducerMap: ReducerMap<T>, initialState?: T): Reducer<T>;
 }
+
+declare module 'redux-actions' {
+  export = ReduxActions;
+}
+
